9b63d143d343cd98fb81ccb7ddbdfe15008718ad
MOhamed Thabet <mohamedthabetabdelhalem_sd23@fcis.bsu.edu.eg>
2024-09-12 01:46:38 +0300

    handling home , footer and header

--- include/footer.php
+++ include/footer.php
@@ -8,9 +8,9 @@
                 </div>
                 <div class="col-md-6 text-md-end">
                     <div class="social-icons">
-                        <a href="https://github.com/yourusername" target="_blank"><i class="fab fa-github"></i></a>
-                        <a href="https://linkedin.com/in/yourusername" target="_blank"><i class="fab fa-linkedin"></i></a>
-                        <a href="https://twitter.com/yourusername" target="_blank"><i class="fab fa-twitter"></i></a>
+                        <a href="https://github.com/MohamedThabt" target="_blank"><i class="fab fa-github"></i></a>
+                        <a href="https://www.linkedin.com/in/mohamed-thabet-5694462a0?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BJVmhEe2rRsGWlP7HkwKDyg%3D%3D" target="_blank"><i class="fab fa-linkedin"></i></a>
+                        <a href="https://x.com/Mohamed13546660/" target="_blank"><i class="fab fa-twitter"></i></a>
                     </div>
                 </div>
             </div>
--- index.php
+++ index.php
@@ -27,6 +27,30 @@ try {
 }
 ?>
 
+
+<!-- if user didn't logined -->
+<?php if(!isset($_SESSION['user_name'])):?>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+            <a href="login.php" class="hover-anchor d-block">
+                <img src="assets/Login-amico.png" alt="Login" class="img-fluid rounded" style="max-height: 300px; object-fit: cover;">
+                <div class="overlay rounded"></div>
+            </a>
+        </div>
+        <div class="col-md-6">
+            <a href="register.php" class="hover-anchor d-block">
+                <img src="assets/Sign up-rafiki.png" alt="Register" class="img-fluid rounded" style="max-height: 300px; object-fit: cover;">
+                <div class="overlay rounded"></div>
+            </a>
+        </div>
+    </div>
+</div>
+<?php endif;?>
+
+
+<!-- if user logined -->
+<?php if(isset($_SESSION['user_name'])):?>
 <div class="container mt-5">
     <div class="table-responsive">
         <table class="table table-striped table-hover">
@@ -57,7 +81,5 @@ try {
         </table>
     </div>
 </div>
-
-<?php
-include "include/footer.php";
-?>
+<?php endif;?>
+<?php include "include/footer.php";?>
